d018ffd3163f6a80143ef8c142082d67a3c0ca5a
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-01 12:12:39 +0400

    Initial Commit

---
+++ TabBarAnimated/TabBarAnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarAnimatedApp.swift
+//  TabBarAnimated
+//
+//  Created by Hakob Ghlijyan on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TabBarAnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
